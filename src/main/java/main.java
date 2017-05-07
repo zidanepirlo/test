c64eb76633ff6b2e9cb0cf56f59493ecbfb4f7b1
yuan <zidanepirlo@126.com>
2017-05-07 19:21:41 +0800

    commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,9 +5,5 @@ public class main {
 
     public static void main(String[] args) {
         
-        System.out.println("11");
-        System.out.println("2222");
-        System.out.println("3333");
-        System.out.println("4444");
     }
 }
