604300cd52c7b447e5887615b4ccb6aecc11f33a
yuan <zidanepirlo@163.com>
2017-09-16 09:52:51 +0800

    this is my master commit!

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,11 +4,6 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("345");
-        System.out.println("main");
-        System.out.println("bug");
-        System.out.println("this is server commit!");
-        System.out.println("this is server commit 1!");
-        System.out.println("this is server commit 2!");
+        System.out.println("this is my master commit!");
     }
 }
