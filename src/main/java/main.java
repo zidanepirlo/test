0fa789dc5d5d9a39f54220209a8577ecff0d4e20
yuan <zidanepirlo@163.com>
2017-09-16 09:51:20 +0800

    this is bug commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,8 +4,6 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("345");
-        System.out.println("main");
         System.out.println("bug");
     }
 }
