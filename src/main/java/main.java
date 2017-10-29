0cf526c36e7280d390428c5e329642c2635399a9
zidanepirlo <zidanepirlo@126.com>
2017-10-29 08:15:38 +0800

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,5 +6,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("345");
         System.out.println("develop");
+        System.out.println("develop2");
     }
 }
